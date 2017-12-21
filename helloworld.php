52102724726ee4bcb5426683fb069d03bf1595ab
INANI EL HOUSSAIN <inanielhoussain@gmail.com>
2017-12-21 12:05:25 +0000

    commit

---
+++ helloworld.php
@@ -0,0 +1,38 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class HelloWorld extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'helloworld';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'El Houssain INANI';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Say Hello World!');
+        $this->description = $this->l('A simple module to say Hello world, Be Nice.');
+
+    }
+
+    public function install()
+    {
+        if (!parent::install()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getContent()
+    {
+        return 'Hello World!';
+    }
+}
